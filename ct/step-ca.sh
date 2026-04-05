008206289ca15fc85c29ae906d014c6facd9b493
fabianwinda <fabianwinda@gmail.com>
2026-04-05 20:14:31 +0200

    chore: setup fork URLs for fabianwinda

--- ct/step-ca.sh
+++ ct/step-ca.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/fabianwinda/ProxmoxVED/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Joerg Heinemann (heinemannj)
-# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/fabianwinda/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/smallstep/certificates
 
 APP="step-ca"
--- docs/DEFAULTS_SYSTEM_GUIDE.md
+++ docs/DEFAULTS_SYSTEM_GUIDE.md
@@ -716,8 +716,8 @@ EOF
 ### Need More Information?
 
 - 📖 [Main Documentation](../../docs/)
-- 🐛 [Report Issues](https://github.com/community-scripts/ProxmoxVED/issues)
-- 💬 [Discussions](https://github.com/community-scripts/ProxmoxVED/discussions)
+- 🐛 [Report Issues](https://github.com/fabianwinda/ProxmoxVED/issues)
+- 💬 [Discussions](https://github.com/fabianwinda/ProxmoxVED/discussions)
 
 ### Useful Commands
 
